bf9b56ae4f837c1d91796d2c569919a0c4bb9d08
Jhuan <j.pressley87@hotmail.com>
2019-01-11 14:24:33 -0500

    Update jenkins.java

--- jenkins.java
+++ jenkins.java
@@ -4,7 +4,7 @@ public class jenkins {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("My name is [your name]");
+		System.out.println("My name is Jay");
 		System.out.println("I am building this project all on my own");
 	}
 
